3a67740d6d6fb757ce7b570c27323dffc4ff504b
donad777 <121440572+donad777@users.noreply.github.com>
2025-02-12 14:46:11 +0100

    Chatsingle

---
+++ scr/Screens/Chat/Group.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import GroupS from '../../Styles/Chat/GroupS'
+const Group = () => {
+  return (
+    <View>
+      <Text>Group</Text>
+    </View>
+  )
+}
+
+export default Group
