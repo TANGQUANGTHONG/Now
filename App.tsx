e68059b60efba36bcfee32a41887e17133d3db62
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2025-02-20 00:02:28 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -11,24 +11,15 @@ const App = () => {
 
   const [chats, setChats] = useState([]); 
 
-  // useEffect(() => {
-  //   connectDb().then(() => {
-  //     getChatsFromSQLite((data) => {
-  //       console.log('Dữ liệu lấy từ SQLite:\n', JSON.stringify(data, null, 2));
-  //       setChats(data);
-  //     });
-  //   });
-  // }, []);
+  useEffect(() => {
+    connectDb().then(() => {
+      getChatsFromSQLite((data) => {
+        console.log('Dữ liệu lấy từ SQLite:\n', JSON.stringify(data, null, 2));
+        setChats(data);
+      });
+    });
+  }, []);
 
-  // useEffect(() => {
-  //   PushNotification.localNotification({
-  //     channelId: 'default-channel',
-  //     title: ' Tin nhắn mới!',
-  //     message: 'Đây là thông báo test!', // Cần có `message`
-  //     playSound: true,
-  //     soundName: 'default',
-  //   });
-  // }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
