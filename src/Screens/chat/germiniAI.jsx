70d74e9678498f4310e3f4929ca3bb659757a568
TANGQUANGTHONG <quangthong2808@gmail.com>
2025-02-25 14:46:42 +0700

    mới merge Canh Tai Do

---
+++ src/Screens/chat/germiniAI.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const germiniAI = () => {
+  return (
+    <View>
+      <Text>germiniAI</Text>
+    </View>
+  )
+}
+
+export default germiniAI
+
+const styles = StyleSheet.create({})
